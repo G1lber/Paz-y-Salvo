b7190237252978621a73bbf746d0aed91c9634b6
G1lber <martinezfrancogilber@gmail.com>
2025-06-19 19:32:52 -0500

    Login usuario y login administrativo

--- pazysalvo/mainapp/urls.py
+++ pazysalvo/mainapp/urls.py
@@ -3,10 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.custom_login, name='login'),
+    path('', views.index, name='index'),
+    path('login/', views.custom_login, name='login'),
     path('pazysalvo/', views.pazysalvo, name='pazysalvo'),
     path('inicio/', views.inicio, name='inicio'),
     path('aprendices/', views.aprendices, name='aprendices'),
     path('prestarlibro/', views.prestarlibro, name='prestar-libro'),
+    path('pendientes/', views.pendientes, name='pendientes'),
     path('pendientes/', views.pendientes, name='pendientes'),
 ]
--- pazysalvo/mainapp/views.py
+++ pazysalvo/mainapp/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth import authenticate, login
 from django.contrib import messages
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
 def custom_login(request):
     return render(request, 'login.html')
 

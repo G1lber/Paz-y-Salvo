cbec2e89efd462ce0bb262e6f62bcb695dac40a7
G1lber <martinezfrancogilber@gmail.com>
2025-08-11 08:36:45 -0500

    Cambio de tipodoc en Admin django

--- pazysalvo/mainapp/admin.py
+++ pazysalvo/mainapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Usuario, Login, Ficha, Roles
+from .models import Usuario, Login, Ficha, Roles,TipoDoc
 
 admin.site.register(Ficha)
 admin.site.register(Roles)
 admin.site.register(Usuario)
+admin.site.register(TipoDoc)
 admin.site.register(Login)
 # Register your models here.

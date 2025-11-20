88281eacce864f5b78364c4a93bd5f9df00d6585
G1lber <martinezfrancogilber@gmail.com>
2025-11-19 23:50:35 -0500

    Cambio para que solo puedan  ingresar los usuarios con rol aprendiz

--- pazysalvo/mainapp/views.py
+++ pazysalvo/mainapp/views.py
@@ -24,17 +24,21 @@ from datetime import date
 # Create your views here.
 def index(request):
     if request.method == "POST":
-        documento = request.POST.get("username")
+        num_doc = request.POST.get("username")
 
         try:
-            usuario = Usuario.objects.get(num_doc=documento)
-            # Guardamos el ID del usuario en sesión
-            request.session["usuario_id"] = usuario.id
-            return redirect("pazysalvo")
+            usuario = Usuario.objects.get(num_doc=num_doc)
         except Usuario.DoesNotExist:
-            return render(request, "index.html", {
-                "error": "El número de documento no está registrado."
-            })
+            return render(request, "index.html", {"error": "Documento no registrado"})
+
+        # Validar rol Aprendiz
+        if usuario.id_rol_FK.nombre_rol != "Aprendiz":
+            return render(request, "index.html", {"error": "Solo los aprendices pueden ingresar"})
+
+        # Guardamos el ID en sesión
+        request.session["usuario_id"] = usuario.id
+
+        return redirect("pazysalvo")
 
     return render(request, "index.html")
 
